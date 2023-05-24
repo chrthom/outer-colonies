fab43015884dbc7baff4ec122a209bcf337cbd88
Christopher Thomsen <christopher.thomsen@christopher-thomsen.net>
2023-05-24 12:40:57 +0200

    Add all 1 part station hull cards

--- backend/src/components/cards/collection/card_collection.ts
+++ backend/src/components/cards/collection/card_collection.ts
@@ -5,12 +5,16 @@ import { Card130, Card166 } from './equipment_weapon_cards';
 import { Card135 } from './infrastructure_action_cards';
 import { Card185, Card242, Card350, Card453 } from './infrastructure_energy_cards';
 import { Card141, Card232 } from './tactic_economy_cards';
+import { Card119, Card152, Card153, Card228, Card446 } from './hull_staton_cards';
 
 export default class CardCollection {
     static cardColony = new ColonyCard();
+    static card119 = new Card119();
     static card130 = new Card130();
     static card135 = new Card135();
     static card141 = new Card141();
+    static card152 = new Card152();
+    static card153 = new Card153();
     static card160 = new Card160();
     static card163 = new Card163();
     static card166 = new Card166();
@@ -18,6 +22,7 @@ export default class CardCollection {
     static card185 = new Card185();
     static card186 = new Card186();
     static card220 = new Card220();
+    static card228 = new Card228();
     static card232 = new Card232();
     static card242 = new Card242();
     static card243 = new Card243();
@@ -27,6 +32,7 @@ export default class CardCollection {
     static card351 = new Card351();
     static card436 = new Card436();
     static card439 = new Card439();
+    static card446 = new Card446();
     static card450 = new Card450();
     static card453 = new Card453();
 }
