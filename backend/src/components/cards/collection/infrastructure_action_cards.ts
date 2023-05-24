f1834fe4dba8dc5e478575a80b44d6110a930a56
Christopher Thomsen <christopher.thomsen@christopher-thomsen.net>
2023-05-24 13:46:42 +0200

    Add all phase 1 action infrastructure cards

--- backend/src/components/cards/collection/infrastructure_action_cards.ts
+++ backend/src/components/cards/collection/infrastructure_action_cards.ts
@@ -3,6 +3,10 @@ import Player from "../../game_state/player";
 import ActionPool, { CardAction } from "../action_pool";
 import InfrastructureCard from "../types/infrastructure_card";
 
+function addToActionPool(player: Player, card: InfrastructureCard) {
+    player.actionPool.push(...card.actionPool().getPool());
+}
+
 export class Card135 extends InfrastructureCard {
     constructor() {
         super(
@@ -23,9 +27,91 @@ export class Card135 extends InfrastructureCard {
         )
     }
     immediateEffect(player: Player) {
-        player.actionPool.push(...this.actionPool().getPool());
+        addToActionPool(player, this);
     }
     actionPool(): ActionPool {
         return new ActionPool(new CardAction(CardType.Hull));
+    }
+}
+
+export class Card154 extends InfrastructureCard {
+    constructor() {
+        super(
+            154, 
+            'Bergbauaußenposten',
+            2,
+            {
+                energy: -1,
+                hp: 0,
+                speed: 0,
+                theta: 0,
+                xi: 0,
+                phi: 0,
+                omega: 0,
+                delta: 0,
+                psi: -1
+            }
+        )
+    }
+    immediateEffect(player: Player) {
+        addToActionPool(player, this);
+    }
+    actionPool(): ActionPool {
+        return new ActionPool(new CardAction(CardType.Infrastructure));
+    }
+}
+
+export class Card164 extends InfrastructureCard {
+    constructor() {
+        super(
+            164,
+            'Rüstungsschmiede',
+            2,
+            {
+                energy: -2,
+                hp: 0,
+                speed: 0,
+                theta: 0,
+                xi: 0,
+                phi: 0,
+                omega: 0,
+                delta: 0,
+                psi: -1
+            }
+        )
+    }
+    immediateEffect(player: Player) {
+        addToActionPool(player, this);
+    }
+    actionPool(): ActionPool {
+        return new ActionPool(new CardAction(CardType.Equipment));
+    }
+}
+
+export class Card183 extends InfrastructureCard {
+    constructor() {
+        super(
+            183, 
+            'Industriekomplex',
+            1,
+            {
+                energy: -4,
+                hp: 0,
+                speed: 0,
+                theta: 0,
+                xi: 0,
+                phi: 0,
+                omega: 0,
+                delta: 0,
+                psi: 0
+            }
+        )
+        this.onlyAttachableToColony = true;
+    }
+    immediateEffect(player: Player) {
+        addToActionPool(player, this);
+    }
+    actionPool(): ActionPool {
+        return new ActionPool(new CardAction(CardType.Equipment, CardType.Infrastructure, CardType.Hull));
     }
 }
