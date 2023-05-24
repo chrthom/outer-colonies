1cbfdfe3989a29d36d873d94ae8f5677fa7ce1f2
Christopher Thomsen <christopher.thomsen@christopher-thomsen.net>
2023-05-24 13:02:22 +0200

    Add all phase 1 defense equipment cards

--- backend/src/components/cards/collection/card_collection.ts
+++ backend/src/components/cards/collection/card_collection.ts
@@ -1,6 +1,6 @@
 import ColonyCard from '../types/colony_card';
 import { Card160, Card186, Card220, Card243, Card342, Card348, Card351, Card436, Card439, Card450 } from './hull_1_part_cards';
-import { Card163, Card170 } from './equipment_defense_cards';
+import { Card103, Card140, Card163, Card170, Card171, Card184, Card240, Card349, Card406, Card447 } from './equipment_defense_cards';
 import { Card130 } from './equipment_weapon_point_defense_cards';
 import { Card135 } from './infrastructure_action_cards';
 import { Card185, Card242, Card350, Card453 } from './infrastructure_energy_cards';
@@ -11,10 +11,12 @@ import { Card126, Card151, Card179, Card180, Card223, Card310, Card420 } from '.
 
 export default class CardCollection {
     static cardColony = new ColonyCard();
+    static card103 = new Card103();
     static card119 = new Card119();
     static card126 = new Card126();
     static card130 = new Card130();
     static card135 = new Card135();
+    static card140 = new Card140();
     static card141 = new Card141();
     static card151 = new Card151();
     static card152 = new Card152();
@@ -23,25 +25,31 @@ export default class CardCollection {
     static card163 = new Card163();
     static card166 = new Card166();
     static card170 = new Card170();
+    static card171 = new Card171();
     static card179 = new Card179();
     static card180 = new Card180();
+    static card184 = new Card184();
     static card185 = new Card185();
     static card186 = new Card186();
     static card220 = new Card220();
     static card223 = new Card223();
     static card228 = new Card228();
     static card232 = new Card232();
+    static card240 = new Card240();
     static card242 = new Card242();
     static card243 = new Card243();
     static card310 = new Card310();
     static card342 = new Card342();
     static card348 = new Card348();
+    static card349 = new Card349();
     static card350 = new Card350();
     static card351 = new Card351();
+    static card406 = new Card406();
     static card420 = new Card420();
     static card436 = new Card436();
     static card439 = new Card439();
     static card446 = new Card446();
+    static card447 = new Card447();
     static card450 = new Card450();
     static card453 = new Card453();
 }
--- backend/src/components/cards/collection/equipment_defense_cards.ts
+++ backend/src/components/cards/collection/equipment_defense_cards.ts
@@ -1,5 +1,55 @@
 import EquipmentCard from '../types/equipment_card';
 
+export class Card103 extends EquipmentCard {
+    constructor() {
+        super(
+            103, 
+            'Adamantiumpanzer',
+            5,
+            {
+                energy: 0,
+                hp: 0,
+                speed: 0,
+                pointDefense: 0,
+                shield: 0,
+                armour: 3,
+                theta: 0,
+                xi: 0,
+                phi: 0,
+                omega: -2,
+                delta: 0,
+                psi: 0
+            },
+            false
+        )
+    }
+}
+
+export class Card140 extends EquipmentCard {
+    constructor() {
+        super(
+            140,
+            'Deflektorschilde',
+            2,
+            {
+                energy: -2,
+                hp: 0,
+                speed: 0,
+                pointDefense: 0,
+                shield: -2,
+                armour: 0,
+                theta: 0,
+                xi: 0,
+                phi: 0,
+                omega: -1,
+                delta: 0,
+                psi: 0
+            },
+            true
+        )
+    }
+}
+
 export class Card163 extends EquipmentCard {
     constructor() {
         super(
@@ -42,6 +92,156 @@ export class Card170 extends EquipmentCard {
                 xi: 0,
                 phi: 0,
                 omega: -1,
+                delta: 0,
+                psi: 0
+            },
+            true
+        )
+    }
+}
+
+export class Card171 extends EquipmentCard {
+    constructor() {
+        super(
+            171, 
+            'Titanplattenplanzer',
+            1,
+            {
+                energy: 0,
+                hp: 0,
+                speed: 0,
+                pointDefense: 0,
+                shield: 0,
+                armour: 2,
+                theta: 0,
+                xi: 0,
+                phi: 0,
+                omega: -1,
+                delta: 0,
+                psi: 0
+            },
+            false
+        )
+    }
+}
+
+export class Card184 extends EquipmentCard {
+    constructor() {
+        super(
+            184, 
+            'Rumpferweiterung',
+            1,
+            {
+                energy: 0,
+                hp: 4,
+                speed: 0,
+                pointDefense: 0,
+                shield: 0,
+                armour: 0,
+                theta: 0,
+                xi: 0,
+                phi: 0,
+                omega: -1,
+                delta: 0,
+                psi: 0
+            },
+            false
+        )
+    }
+}
+
+export class Card240 extends EquipmentCard {
+    constructor() {
+        super(
+            240, 
+            'Ceramo-Stahl',
+            1,
+            {
+                energy: 0,
+                hp: 3,
+                speed: -1,
+                pointDefense: 0,
+                shield: 0,
+                armour: 1,
+                theta: 0,
+                xi: 0,
+                phi: 0,
+                omega: -1,
+                delta: 0,
+                psi: 0
+            },
+            false
+        )
+    }
+}
+
+export class Card349 extends EquipmentCard {
+    constructor() {
+        super(
+            349, 
+            'Dura-Stahl',
+            1,
+            {
+                energy: 0,
+                hp: 1,
+                speed: 0,
+                pointDefense: 0,
+                shield: 0,
+                armour: 2,
+                theta: 0,
+                xi: 0,
+                phi: 0,
+                omega: -2,
+                delta: 0,
+                psi: 0
+            },
+            false
+        )
+    }
+}
+
+export class Card406 extends EquipmentCard {
+    constructor() {
+        super(
+            406, 
+            'Kohlenstoffsilikatpanzer',
+            4,
+            {
+                energy: 0,
+                hp: 3,
+                speed: 0,
+                pointDefense: 0,
+                shield: 0,
+                armour: 2,
+                theta: 0,
+                xi: 0,
+                phi: 0,
+                omega: -2,
+                delta: 0,
+                psi: 0
+            },
+            false
+        )
+    }
+}
+
+export class Card447 extends EquipmentCard {
+    constructor() {
+        super(
+            447,
+            'Schildgondel',
+            1,
+            {
+                energy: 0,
+                hp: 1,
+                speed: 0,
+                pointDefense: 0,
+                shield: 1,
+                armour: 0,
+                theta: 0,
+                xi: 0,
+                phi: 0,
+                omega: -2,
                 delta: 0,
                 psi: 0
             },
