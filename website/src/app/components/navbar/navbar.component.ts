36b655a4e251d16f0ffc143f2ecd8db741bc7f4c
Christopher Thomsen <christopher.thomsen@christopher-thomsen.net>
2023-06-28 19:34:04 +0200

    Close #53 Login and logout

--- website/src/app/components/navbar/navbar.component.ts
+++ website/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import AuthService from 'src/app/auth.service';
 
 @Component({
-  selector: 'app-navbar',
+  selector: 'oc-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-
+  constructor(private router: Router, private authService: AuthService) {}
+  logout() {
+    this.authService.logout();
+    this.router.navigate([ '/login' ]);
+  }
 }
