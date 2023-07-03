9bbf0630e332997192ef55a20f0c89620e64a877
Christopher Thomsen <christopher.thomsen@christopher-thomsen.net>
2023-07-03 13:45:01 +0200

    #58 Deck builder - Display available cards

--- website/src/app/pages/deck/deck.component.ts
+++ website/src/app/pages/deck/deck.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import ApiService from 'src/app/api.service';
 import AuthService from 'src/app/auth.service';
+import { DeckCard } from '../../../../../server/src/components/shared_interfaces/rest_api';
 
 @Component({
   selector: 'oc-page-deck',
   templateUrl: './deck.component.html',
   styleUrls: ['./deck.component.scss']
 })
-export class DeckPage {
-  constructor(private apiService: ApiService, private authService: AuthService) {
-    apiService.listDeck(authService.sessionToken).subscribe(console.log); ////
+export class DeckPage implements OnInit {
+  activeCards!: DeckCard[];
+  reserveCards!: DeckCard[];
+  constructor(private apiService: ApiService, private authService: AuthService) {}
+  ngOnInit(): void {
+    this.reload();
+  }
+  reload() {
+    this.apiService.listDeck(this.authService.sessionToken).subscribe(res => {
+      if (res) {
+        this.activeCards = res.activeCards;
+        this.reserveCards = res.reserveCards;
+      }
+    });
   }
 }
