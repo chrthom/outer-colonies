9fcd9deaa0a41206aa9ca23a81c88d150b19eb9a
Christopher Thomsen <christopher.thomsen@christopher-thomsen.net>
2023-07-05 09:52:15 +0200

    Fix AP URL in website config

--- website/src/environments/environment.ts
+++ website/src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
     stage: 'production',
     url: {
         game: 'https://game.outercolonies.thomsen.in',
-        api: 'http://api.outercolonies.thomsen.in',
+        api: 'https://api.outercolonies.thomsen.in',
         assets: 'https://assets.outercolonies.thomsen.in'
     }
 };
