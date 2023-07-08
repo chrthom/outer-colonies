724da1765fdbf4dc01cb914c3ce9ac02f71e3dae
Christopher Thomsen <christopher.thomsen@christopher-thomsen.net>
2023-07-08 17:05:10 +0200

    Set colony HP back to 50

--- server/src/components/config/rules.ts
+++ server/src/components/config/rules.ts
@@ -1,7 +1,7 @@
 class OCRules {
     readonly cardsToDrawPerTurn = 2;
     readonly cardsPerMission = 2;
-    readonly colonyHP = 1; // Just for testing
+    readonly colonyHP = 50;
     readonly initialCardsToDraw = 7;
     readonly maxHandCards = 7;
     readonly maxRange = 4;
