650f340e10c660e16b884c3e1863ef696335a9f2
Christopher Thomsen <christopher.thomsen@christopher-thomsen.net>
2023-07-10 15:58:38 +0200

    #109 Set env for client

--- client/src/config/environment.ts
+++ client/src/config/environment.ts
@@ -6,4 +6,12 @@ class DevelopmentEnvironment {
     }
 }
 
-export const environment = new DevelopmentEnvironment();
+class ProductionEnvironment {
+    name = 'development';
+    urls = {
+        website: 'https://www.outercolonies.thomsen.in',
+        api: 'https://api.outercolonies.thomsen.in'
+    }
+}
+
+export const environment = new ProductionEnvironment();
