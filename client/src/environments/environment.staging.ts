712547b6fbf267fa0a18e05a28b422297dcb7c2c
Christopher Thomsen <christopher.thomsen@christopher-thomsen.net>
2023-07-13 09:28:04 +0200

    #93 Configurations for staging environment

---
+++ client/src/environments/environment.staging.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    name: 'production',
+    urls: {
+        website: 'https://staging.outercolonies.de',
+        api: 'https://api.staging.outercolonies.de'
+    }
+};
--- website/src/environments/environment.ts
+++ website/src/environments/environment.ts
@@ -2,8 +2,8 @@ export const environment = {
     stage: 'production',
     https: true,
     url: {
-        game: 'https://game.outercolonies.thomsen.in',
-        api: 'https://api.outercolonies.thomsen.in',
-        assets: 'https://assets.outercolonies.thomsen.in'
+        game: 'https://game.outercolonies.de',
+        api: 'https://api.outercolonies.de',
+        assets: 'https://assets.outercolonies.de'
     }
 };
