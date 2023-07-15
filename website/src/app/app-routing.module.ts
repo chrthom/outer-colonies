f70cf3a80e89115620403094e02b6eac0a42b3ab
Christopher Thomsen <christopher.thomsen@christopher-thomsen.net>
2023-07-15 13:18:54 +0200

    Fix reroute to HTTPS

--- website/src/app/app-routing.module.ts
+++ website/src/app/app-routing.module.ts
@@ -1,41 +1,59 @@
 import { NgModule, inject } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivateFn, PreloadAllModules, RouterModule, RouterStateSnapshot, Routes } from '@angular/router';
-import { AuthGuard } from './auth.guard';
+import { CanActivateFn, PreloadAllModules, Router, RouterModule, Routes } from '@angular/router';
 import { LoginPage } from './pages/login/login.page';
 import { HomePage } from './pages/home/home.page';
 import { RegisterPage } from './pages/register/register.page';
-import { Observable } from 'rxjs';
+import { tap } from 'rxjs';
 import { DeckPage } from './pages/deck/deck.component';
 import { ImprintComponent as ImprintPage } from './pages/imprint/imprint.page';
 import { DataPrivacyComponent as DataPrivacyPage } from './pages/data-privacy/data-privacy.page';
+import AuthService from './auth.service';
+import { environment } from 'src/environments/environment';
 
-const canActivateFn: CanActivateFn =
-  (next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> => inject(AuthGuard).canActivate(next, state);
+const authGuardFn: CanActivateFn = () => {
+  return inject(AuthService).check().pipe(tap(b => !b ? inject(Router).navigate([ '/login' ]) : {}))
+};
+const httpsGuardFn: CanActivateFn = () => {
+  if (!environment.https || window.location.protocol == "https:") return true;
+  else {
+    window.location.protocol = "https:";
+    window.location.reload();
+    return false;
+  }
+};
+
 const routes: Routes = [
   {
-    path: 'login',
-    component: LoginPage
-  }, {
-    path: 'register',
-    component: RegisterPage
-  }, {
-    path: 'deck',
-    canActivate: [canActivateFn],
-    component: DeckPage
-  }, {
-    path: 'imprint',
-    component: ImprintPage
-  }, {
-    path: 'privacy',
-    component: DataPrivacyPage
-  }, {
     path: '',
-    canActivate: [canActivateFn],
-    component: HomePage,
-    pathMatch: 'full'
-  }, {
-    path: '**',
-    redirectTo: ''
+    canActivate: [ httpsGuardFn ],
+    canActivateChild: [ httpsGuardFn ],
+    children: [
+      {
+        path: 'login',
+        component: LoginPage
+      }, {
+        path: 'register',
+        component: RegisterPage
+      }, {
+        path: 'deck',
+        canActivate: [ authGuardFn ],
+        component: DeckPage
+      }, {
+        path: 'imprint',
+        component: ImprintPage
+      }, {
+        path: 'privacy',
+        component: DataPrivacyPage
+      }, {
+        path: '',
+        canActivate: [ authGuardFn ],
+        component: HomePage,
+        pathMatch: 'full'
+      }, {
+        path: '**',
+        redirectTo: ''
+      }
+    ]
   }
 ];
 
