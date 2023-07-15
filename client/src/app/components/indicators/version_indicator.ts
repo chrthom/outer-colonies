2d4dbdb26e814e7ca7e79690c3b630b0d5921c62
Christopher Thomsen <christopher.thomsen@christopher-thomsen.net>
2023-07-15 20:26:15 +0200

    Bump version to 1.1.1

--- client/src/app/components/indicators/version_indicator.ts
+++ client/src/app/components/indicators/version_indicator.ts
@@ -5,7 +5,7 @@ export default class VersonIndicator {
         scene.add.text(
             layout.version.x,
             layout.version.y,
-            'Outer Colonies v1.1 - Enceladus'
+            'Outer Colonies v1.1.1 - Enceladus'
         )
             .setFontSize(layout.font.size)
             .setFontFamily(layout.font.captionFamily)
