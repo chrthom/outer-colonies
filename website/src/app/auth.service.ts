7ac6c68baca5c4f4498207c48daf3735a9e0c6a4
Christopher Thomsen <christopher.thomsen@christopher-thomsen.net>
2023-07-16 17:07:32 +0200

    Fix stay logged in functon

--- website/src/app/auth.service.ts
+++ website/src/app/auth.service.ts
@@ -22,14 +22,12 @@ export default class AuthService {
   login(username: string, password: string, remember: boolean): Observable<boolean> {
     this.username = username;
     this.password = password;
-    const loginAttempt = this.check();
-    loginAttempt.pipe(tap((success) => {
+    return this.check().pipe(tap((success) => {
       if (success && remember) {
         this.cookieService.set('u', username, this.cookieExpiry);
         this.cookieService.set('p', password, this.cookieExpiry);
       }
     }));
-    return loginAttempt;
   }
 
   logout() {
