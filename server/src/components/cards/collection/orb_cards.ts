0c0634e4abf083f40df0bc4da89c3e64466819e5
Thomsen, Christopher <christopher.thomsen@adesso.de>
2023-09-14 09:53:11 +0200

    #176 Add card 146

--- server/src/components/cards/collection/orb_cards.ts
+++ server/src/components/cards/collection/orb_cards.ts
@@ -1,7 +1,35 @@
-import { CardType, TacticDiscipline } from "../../config/enums";
+import { TacticDiscipline } from "../../config/enums";
 import Player from "../../game_state/player";
 import OrbCard from "../types/orb_card";
-import TacticCard from "../types/tactic_card";
+
+export class Card146 extends OrbCard {
+  constructor() {
+    super(
+      146,
+      'Europa',
+      2,
+      {
+        energy: 0,
+        hp: 0,
+        speed: 0,
+        pointDefense: 0,
+        shield: 0,
+        armour: 0,
+        theta: 0,
+        xi: 0,
+        phi: 0,
+        omega: 0,
+        delta: 0,
+        psi: 0
+      },
+      [ TacticDiscipline.Economy ]
+    );
+  }
+  override onStartTurn(player: Player): void {
+    this.additionalCardWhenDrawing(TacticDiscipline.Intelligence, player);
+  }
+  override onEndTurn(): void {}
+}
 
 export class Card159 extends OrbCard {
   constructor() {
@@ -27,10 +55,7 @@ export class Card159 extends OrbCard {
     );
   }
   override onStartTurn(player: Player): void {
-    const relevantCardDrawn = this.getDrawnCards(player).some(c =>
-      c.type == CardType.Tactic && (c as TacticCard).discipline == TacticDiscipline.Intelligence
-    );
-    if (relevantCardDrawn) player.drawCards(1);
+    this.additionalCardWhenDrawing(TacticDiscipline.Science, player);
   }
   override onEndTurn(): void {}
 }
--- server/src/components/cards/types/orb_card.ts
+++ server/src/components/cards/types/orb_card.ts
@@ -1,10 +1,11 @@
-import { CardSubtype, CardType } from '../../config/enums';
+import { CardSubtype, CardType, TacticDiscipline } from '../../config/enums';
 import { rules } from '../../config/rules';
 import Player from '../../game_state/player';
 import ActionPool, { CardAction } from '../action_pool';
 import Card from '../card';
 import CardProfile, { OrbProfile } from '../card_profile';
 import CardStack from '../card_stack';
+import TacticCard from './tactic_card';
 
 export default abstract class OrbCard extends Card {
   readonly orbProfile!: OrbProfile;
@@ -35,7 +36,16 @@ export default abstract class OrbCard extends Card {
   override get actionPool(): ActionPool {
     return new ActionPool(...this.actionPoolCardTypes.map(ct => new CardAction(...ct)));
   }
-  protected getDrawnCards(player: Player) {
+  protected additionalCardWhenDrawing(cardType: CardSubtype, player: Player) {
+    const relevantCardDrawn = this.getDrawnCards(player).some(c =>
+      c.type == cardType || this.isTacticDiscipline(cardType) && c.type == CardType.Tactic && (c as TacticCard).discipline == cardType
+    );
+    if (relevantCardDrawn) player.drawCards(1);
+  }
+  private isTacticDiscipline(cardType: CardSubtype): boolean {
+    return Object.values(TacticDiscipline).map(td => <CardSubtype> td).includes(cardType);
+  }
+  private getDrawnCards(player: Player) {
     return player.hand.slice(-rules.cardsToDrawPerTurn).map(c => c.card);
   }
 }
