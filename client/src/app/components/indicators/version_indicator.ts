9d1e0f220f26670c7a5b1fbea458f6f2863cf38e
Thomsen, Christopher <christopher.thomsen@adesso.de>
2024-05-20 16:47:41 +0200

    Increment patch version to 2.1.1

--- client/src/app/components/indicators/version_indicator.ts
+++ client/src/app/components/indicators/version_indicator.ts
@@ -8,7 +8,7 @@ export default class VersonIndicator {
       .text(
         layoutConfig.load.version.x,
         layoutConfig.load.version.y,
-        `Janus (v2.1.0)${environment.stage != 'production' ? `\nenv: ${environment.stage}` : ''}`
+        `Janus (v2.1.1)${environment.stage != 'production' ? `\nenv: ${environment.stage}` : ''}`
       )
       .setFontSize(layoutConfig.fontSize.normal)
       .setFontFamily(designConfig.fontFamily.caption)
