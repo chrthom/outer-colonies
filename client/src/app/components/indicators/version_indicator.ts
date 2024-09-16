94a64054655b3ef96af0de041c119a8b840a628e
Thomsen, Christopher <christopher.thomsen@adesso.de>
2024-09-16 20:19:15 +0200

    Bump patch version to 2.2.1

--- client/src/app/components/indicators/version_indicator.ts
+++ client/src/app/components/indicators/version_indicator.ts
@@ -8,7 +8,7 @@ export default class VersonIndicator {
       .text(
         layoutConfig.load.version.x,
         layoutConfig.load.version.y,
-        `Telesto (v2.2.0)${environment.stage != 'production' ? `\nenv: ${environment.stage}` : ''}`
+        `Telesto (v2.2.1)${environment.stage != 'production' ? `\nenv: ${environment.stage}` : ''}`
       )
       .setFontSize(layoutConfig.fontSize.normal)
       .setFontFamily(designConfig.fontFamily.caption)
