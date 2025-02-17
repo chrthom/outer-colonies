902111f8ef3a6668e36cdf8ad7c2c2d3f78809e4
Christopher Thomsen <christopher.thomsen@adesso.de>
2025-02-17 13:36:08 +0100

    Bump patch version to 2.3.2

--- client/src/app/components/indicators/version_indicator.ts
+++ client/src/app/components/indicators/version_indicator.ts
@@ -8,7 +8,7 @@ export default class VersonIndicator {
       .text(
         layoutConfig.load.version.x,
         layoutConfig.load.version.y,
-        `Calypso (v2.3.1)${environment.stage != 'production' ? `\nenv: ${environment.stage}` : ''}`
+        `Calypso (v2.3.2)${environment.stage != 'production' ? `\nenv: ${environment.stage}` : ''}`
       )
       .setFontSize(layoutConfig.fontSize.normal)
       .setFontFamily(designConfig.fontFamily.caption)
