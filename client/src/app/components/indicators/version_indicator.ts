d5b956325d5dda7c1c767d5fca6d0fa0f79873ef
Christopher Thomsen <github@christopher-thomsen.net>
2026-05-15 09:38:06 +0200

    chore: bump version to v3.1.0 - Pandora

--- client/src/app/components/indicators/version_indicator.ts
+++ client/src/app/components/indicators/version_indicator.ts
@@ -8,7 +8,7 @@ export default class VersonIndicator {
       .text(
         layoutConfig.load.version.x,
         layoutConfig.load.version.y,
-        `Prometheus (v3.0.0)${environment.stage != 'production' ? `\nenv: ${environment.stage}` : ''}`
+        `Pandora (v3.1.0)${environment.stage != 'production' ? `\nenv: ${environment.stage}` : ''}`
       )
       .setFontSize(layoutConfig.fontSize.normal)
       .setFontFamily(designConfig.fontFamily.caption)
