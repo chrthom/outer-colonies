a82f56f95fceb282bcfe6f8ee7bdc3d081263741
Christopher Thomsen <github@christopher-thomsen.net>
2026-05-16 10:26:03 +0200

    fix(ci): fix git diff working directory for format detection
    - Fix git diff to run from subproject directory
    - Add trailing comma in client/main.ts for format test

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,6 +2,6 @@ import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
 
-    bootstrapApplication(AppComponent, {
-  providers: [importProvidersFrom(BrowserModule),],
-}).catch(err=>console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(BrowserModule),]
+}).catch(err => console.error(err));
