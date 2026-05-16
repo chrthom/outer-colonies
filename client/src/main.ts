9868f2f258c15efc2713df18d005481e543ddb8c
Christopher Thomsen <github@christopher-thomsen.net>
2026-05-16 10:14:23 +0200

    Formatting issue for testing

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,8 +2,6 @@ import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
 
-// Test comment with bad formatting
-
     bootstrapApplication(AppComponent, {
   providers: [importProvidersFrom(BrowserModule),],
 }).catch(err=>console.error(err));
